3b194b86fd9b3b81a4fb7fcb06d2fe452eae0b27
Sabila Hussain <sabila.hussain127@gmail.com>
2020-10-14 23:48:02 -0400

    created project folder which includes updates for adding a username

--- Project/lib/db.php
+++ Project/lib/db.php
@@ -1,4 +1,3 @@
-
 <?php
 //for this we'll turn on error output so we can try to see any problems on the screen
 //this will be active for any script that includes/requires this one
@@ -13,14 +12,14 @@ function getDB(){
         try{
             //__DIR__ helps get the correct path regardless of where the file is being called from
             //it gets the absolute path to this file, then we append the relative url (so up a directory and inside lib)
-            require_once(__DIR__. "/../lib/config.php");//pull in our credentials
+            require_once(__DIR__. "/config.php");//pull in our credentials
             //use the variables from config to populate our connection
             $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
             //using the PDO connector create a new connect to the DB
             //if no error occurs we're connected
             $db = new PDO($connection_string, $dbuser, $dbpass);
         }
-        catch(Exception $e){
+    catch(Exception $e){
             var_export($e);
             $db = null;
         }
